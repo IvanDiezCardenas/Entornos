8b0d4dae86cf1687e07e20ae2a17b328bfe7d32a
ivan2545 <ivandiezcardenas@gmail.com>
2023-02-13 19:36:04 +0100

    "commit"

--- EvaluableU9/src/Actividad/DobleNumero.java
+++ EvaluableU9/src/Actividad/DobleNumero.java
@@ -1,5 +1,18 @@
 package Actividad;
 
 public class DobleNumero {
-
+//Atributos
+	private int numero;
+//Constructores
+	public DobleNumero(int numero){
+		this.numero = numero;
+	}
+	void setValue(int n) {
+		
+	}
+int getValue() {
+	int resultado = numero * 2;
+	return numero;
+	
+}
 }
---
+++ EvaluableU9/src/Actividad/PreubaDobleNumero.java
@@ -0,0 +1,12 @@
+package Actividad;
+
+public class PreubaDobleNumero {
+
+	public static void main(String[] args) {
+DobleNumero x = new DobleNumero(1);
+System.out.println(x.getValue());
+x.setValue(8);
+System.out.println(x.getValue());
+	}
+
+}
