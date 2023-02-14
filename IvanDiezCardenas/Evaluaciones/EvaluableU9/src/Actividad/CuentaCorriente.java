532e83ac1703b4205c3fda1f1a511dfcba56b8ec
ivan2545 <ivandiezcardenas@gmail.com>
2023-02-14 18:32:44 +0100

    "Ultimo Cambio"

--- IvanDiezCardenas/Evaluaciones/EvaluableU9/src/Actividad/CuentaCorriente.java
+++ IvanDiezCardenas/Evaluaciones/EvaluableU9/src/Actividad/CuentaCorriente.java
@@ -1,19 +1,24 @@
 package Actividad;
 
 public class CuentaCorriente {
-private String saldo;
-public CuentaCorriente(){
-	this.saldo = "0";
-}
+	private String saldo;
 
-public int getSaldo() {
-	return getSaldo();
-	
-}
-void imposicion(int x) {
-	
-}
-void reIntegro(int x) {
-	
-}
+	public CuentaCorriente() {
+		this.saldo = "0";
+	}
+
+	public int getSaldo() {
+		this.saldo = saldo;
+		return getSaldo();
+	}
+
+	void imposicion(int x) {
+		int importe = 0;
+		String ingresar = saldo + importe;
+	}
+
+	void reIntegro(int x) {
+		String importe = "";
+		String reintegro = importe - saldo;
+	}
 }
--- IvanDiezCardenas/Evaluaciones/EvaluableU9/src/Actividad/PruebaCuentaCorriente.java
+++ IvanDiezCardenas/Evaluaciones/EvaluableU9/src/Actividad/PruebaCuentaCorriente.java
@@ -3,10 +3,10 @@ package Actividad;
 public class PruebaCuentaCorriente {
 
 	public static void main(String[] args) {
-PruebaCuentaCorriente cc = new PruebaCuentaCorriente();
-cc.imposicion(200);
-cc.reIntegro(300);
-System.out.println(cc.getSaldo());
+		PruebaCuentaCorriente cc = new PruebaCuentaCorriente();
+		cc.imposicion(200);
+		cc.reIntegro(300);
+		System.out.println(cc.getSaldo());
 	}
 
 }
